8d42dfb2813acc9128f95aed3254794038c461aa
Hristo Hristov <h.hristov2212@gmail.com>
2023-06-28 09:29:06 +0300

    Change terminal interface

--- tests_formatter.sh
+++ tests_formatter.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-# Black: '\033[0;30m'
-# Red: '\033[0;31m'
-# Green: '\033[0;32m'
-# Yellow: '\033[0;33m'
-# Blue: '\033[0;34m'
+# Black:   '\033[0;30m'
+# Red:     '\033[0;31m'
+# Green:   '\033[0;32m'
+# Yellow:  '\033[0;33m'
+# Blue:    '\033[0;34m'
 # Magenta: '\033[0;35m'
-# Cyan: '\033[0;36m'
-# White: '\033[0;37m'
+# Cyan:    '\033[0;36m'
+# White:   '\033[0;37m'
 
 function format_file(){
-    length=${#1}
-    local hashtags=$(printf -- "-%.0s" $(seq 1 "$length"))
-    echo -e "\n--------| $1 |--------"
+
+    local color_formater='\033[0;36m'
+    local color_name_file='\033[0;31m'
+    local reset='\033[0m'
+
+    echo -e "\n"
+    print_lines $1
+    echo -e "\n--------| ${color_name_file}$1${reset} |--------"
+    print_lines $1
+    echo -e "\n"
 
     local extension="${1: -3}"
     if [[ ! $extension == ".py" ]]; then
@@ -20,16 +27,13 @@ function format_file(){
         exit 1
     fi
 
-    local color='\033[0;31m'
-    local reset='\033[0m'
-
-    echo -e "\n${color}@ isort...${reset}"
+    echo -e "\n${color_formater}>_ isort...${reset}"
     isort $1
 
-    echo -e "\n${color}@ black --line-length=140...${reset}"
+    echo -e "\n${color_formater}>_ black --line-length=140...${reset}"
     black --line-length=140 $1
 
-    echo -e "\n${color}@ flake8 --max-line=140...${reset}"
+    echo -e "\n${color_formater}>_ flake8 --max-line=140...${reset}"
     flake8 --max-line=140 $1
 }
 
@@ -45,6 +49,17 @@ function newest_file() {
         fi
     done
     format_file $file_name
+}
+
+function print_lines() {
+  local input="$1"
+  local length=${#input}
+  
+  echo -n "----------"
+  for ((i=0; i<length; i++)); do
+      echo -n "-"
+  done
+  echo -n "----------"
 }
 
 if [ $# -eq 0 ]; then
